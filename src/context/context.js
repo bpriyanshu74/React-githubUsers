583ebff5c0b318a4e1ab6afe635a2c5b8cf3bad0
Priyanshu Bhardwaj <53000551+bpriyanshu74@users.noreply.github.com>
2023-12-29 10:58:02 +0530

    charts added

--- src/context/context.js
+++ src/context/context.js
@@ -8,11 +8,23 @@ const rootUrl = 'https://api.github.com';
 
 const GithubContext = React.createContext()
 
-const GithubProvider = ({children}) => {
+const GithubProvider = ({children}) => {      
 
       const [githubUser,setgithubUser] = useState(mockUser)
       const [repos,setRepos] = useState(mockRepos)
       const [followers,setFollowers] = useState(mockFollowers)
+      const [requests,setRequests] = useState(0)
+      const [loading,setLoading] = useState(false)
+
+      const checkRequest = () => {
+            axios(`${rootUrl}/rate_limit`)
+            .then((res) => {
+                  console.log(res)
+            })
+            .catch((err) => console.log(err))
+      }
+
+      useEffect(checkRequest,[])
 
       return <GithubContext.Provider value={{githubUser,setgithubUser,repos,setRepos,followers,setFollowers}}>
             {children}
